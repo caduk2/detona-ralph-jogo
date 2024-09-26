c8bc10b0c18e698b556517a760523d70ea073417
Carlos Eduardo Tavares <carlos_ed.tavares@hotmail.com>
2024-09-26 17:10:36 -0300

    Adição de elementos visuais, audios, efeitos de estilo, aprimoramento de jogabilidade e sistema de vidas. Implementação de alertas e condições. Planejamento de melhorias futuras.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Game Detona Ralph com HTML, CSS e JavaScript 
+
+## Descrição
+Este projeto consiste em finalizar o Game Detona Ralph utilizando HTML, CSS e JavaScript. O objetivo é incrementar o jogo com recursos audiovisuais e mecânicas de ação, proporcionando uma experiência mais rica e interativa.
+
+## Tecnologias Utilizadas
+- **HTML**: Utilizado para estruturar o conteúdo da página.
+- **CSS**: Utilizado para estilizar e posicionar os elementos da página.
+- **JavaScript**: Utilizado para adicionar interatividade e dinamismo à página.
+
+## Funcionalidades Implementadas
+- **Elementos Visuais**: Adição de elementos como janela fechada e janela aberta com o Ralph, criados no CapCut.
+- **Efeitos de Estilo**: Adição de sombra à janela ao passar o cursor sobre ela.
+- **Aprimoramento de Jogabilidade**: Aumento da velocidade do jogo.
+- **Sistema de Vidas**: Implementação de uma contagem decrescente de vidas.
+
+## Alertas e Condições
+- **"Vem Jogar!"**: Exibido quando não há interação com o jogo e o tempo acaba.
+- **"Parabéns!"**: Exibido quando o jogador faz um ponto ou mais e o tempo acaba, acompanhado de um áudio.
+- **"Game Over!"**: Exibido quando o jogador perde todas as vidas, acompanhado de um áudio.
+
+## Melhorias Futuras
+- **Novos Níveis**: Adição de novos níveis com desafios progressivos.
+- **Personagens Adicionais**: Inclusão de novos personagens.
+- **Placar de Pontuação e Compartilhamento**: Permitir que os jogadores compartilhem suas pontuações nas redes sociais.
+
+## Conclusão
+Este projeto demonstra minhas habilidades em HTML, CSS e JavaScript, além da capacidade de criar uma experiência de usuário envolvente e interativa. Estou ansioso para aplicar essas habilidades em um ambiente profissional e contribuir para o desenvolvimento de projetos inovadores.
---
+++ src/scripts/engine.js
@@ -0,0 +1,122 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+        lives: document.querySelector("#lives"),
+    },
+    values: {
+        timerId: null,
+        gameValocity: 500,
+        hitPosition: 0,
+        result: 0,
+        curretTime: 10,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+};
+
+function countDown () {
+    state.values.curretTime--;
+    state.view.timeLeft.textContent = state.values.curretTime;
+    
+    if ((state.values.curretTime < 1) && (state.values.result > 0)) {
+        resetCountDown();
+        playSound("wins");
+        
+        setTimeout(() => {
+            alert("Parabéns! Sua pontuação foi: " + state.values.result);
+            reset();
+        }, 100); // Atraso de 100ms para garantir que o som comece a tocar
+    } else if (state.values.curretTime < 0) {
+        resetCountDown();
+        alert("Venha jogar !");        
+        reset()          
+    }  
+}
+
+function resetCountDown() {
+    clearInterval(state.actions.countDownTimerId);
+    clearInterval(state.actions.timerId);
+}
+
+function playSound (audioName) {
+    let audio = new Audio(`/src/audios/${audioName}.m4a`);
+    audio.volume = 0.1;
+    audio.play();
+}
+
+let previousSquareIndex = null;
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber;
+    do {
+        randomNumber = Math.floor(Math.random() * 9);
+    } while (randomNumber === previousSquareIndex);
+
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+
+    previousSquareIndex = randomNumber;
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            } else {
+                state.view.lives.textContent--;
+                playSound("miss");
+
+                if (state.view.lives.textContent < 1) {
+                    playSound("end");
+                    
+                    setTimeout(() => {
+                    resetCountDown();
+                    alert("Game Over! O seu resultado foi: " + state.values.result);
+                    reset()                      
+                    }, 100); // Atraso de 100ms para garantir que o som comece a tocar
+                }
+            }            
+        });
+    });
+}
+
+function reset() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    state.actions.timerId = setInterval(randomSquare, 1000);
+    state.actions.countDownTimerId = setInterval(countDown, 1000);
+
+    state.values.lives = 3;
+    state.values.result = 0;
+    state.values.curretTime = 10;
+
+    state.view.score.textContent = state.values.result;
+    state.view.lives.textContent = state.values.lives;
+    state.view.timeLeft.textContent = state.values.curretTime;
+}
+
+function init() {
+    randomSquare();
+    addListenerHitBox();
+
+    state.view.score.textContent = state.values.result;
+    state.view.timeLeft.textContent = state.values.curretTime;
+}
+
+init();
